d51c2dd41e100b4c80ce9b95fcdb7cacbb7c2561
uuu0614 <33905956+uuu0614@users.noreply.github.com>
2021-01-02 03:05:16 +0900

    fixed identfier.c

--- src/identfier.c
+++ src/identfier.c
@@ -12,15 +12,40 @@ typedef struct
     int values[IDENTFIER_LIST_UNIT_SIZE]; //value
 } IdentifierList;
 
-IdentifierList identifiers; // 識別子のリスト
+IdentifierList __identifiers__ = { // 識別子のリスト
+    NULL,
+    {},
+    {}
+};
 
 int __end_of_idefinder_list__ = 0; // 識別子リストの末尾のインデックス。 
-struct IdentifierList *__identfier_list_ptr__; //識別子リストの自己参照の最後のポインタ。 
+IdentifierList *__identfier_list_ptr__; //識別子リストの自己参照の最後のポインタ。 
+int __last_identfier_number__ = 0; // 最後の識別子の番号
+
+void initialize_identifier_list() { // 識別子リストを初期化する。
+    __identfier_list_ptr__ = &__identifiers__;
+}
+
 void add_identfier(char *name) // 識別子を追加する。
 {
     __end_of_idefinder_list__++;
+    __last_identfier_number__++;
     if (__end_of_idefinder_list__ >= IDENTFIER_LIST_UNIT_SIZE) {
         //新規ユニットを登録する。
-        __end_of_idefinder_list__ = 0;
+
+        IdentifierList next_list = { // 新しいリスト。
+            NULL,
+            {},
+            {}
+        };
+        __identfier_list_ptr__->next = &next_list; //次のリストを示すポインタを代入
+        __identfier_list_ptr__ = &next_list;
+        __end_of_idefinder_list__ = 0; // 初期値に戻す
     }
+    // 名前を追加
+    __identfier_list_ptr__->names[__end_of_idefinder_list__] = name;
+    __identfier_list_ptr__->values[__end_of_idefinder_list__] = __last_identfier_number__;
 }
+
+// 識別子情報用に確保したメモリを解放
+void remove_identfier();
