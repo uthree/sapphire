0b1c51776f1a540b7dc5e5f893dc7de91f316f10
uuu0614 <33905956+uuu0614@users.noreply.github.com>
2021-01-13 19:11:32 +0900

    updated initialize_runtime()

--- src/initialize.c
+++ src/initialize.c
@@ -1,5 +1,11 @@
+# pragma once
+# include <stdlib.h>
 # include "identifier.c"
+# include "object.c"
+
+Object sapphire_nil;
 
 void initialize_runtime() {
-    initialize_identifier_list();
+    sapphire_nil.type = nil; // initialize nil object 
+    initialize_identifier_list(); // initialize identifier list. 
 }
